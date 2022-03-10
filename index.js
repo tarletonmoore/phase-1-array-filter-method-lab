506092237168b41360579380e6f60ded76637113
John Tarleton Moore <tarletonmoore@gmail.com>
2022-03-10 17:16:09 -0500

    commit tree 93a3a62151c354f0dd1593e0687b7699ccedf311

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, string) {
+    let list = array.filter(name => name.toUpperCase() === string.toUpperCase())
+    if (list) {
+        return list
+    }
+}
+
+function fuzzyMatch(array, string) {
+    let list = array.filter(name => name.toUpperCase().indexOf(string.toUpperCase()) === 0)
+    return list
+}
+
+function matchName(array, string) {
+    let list = array.filter(obj => obj.name === string)
+    return list
+}
+
+
